34d8c549f41253e2199ce549ab4a2df2d9629880
Vanessa Moreira Wendling <vanessa.wendling@gft.com>
2025-12-27 18:54:48 -0300

    inicializando projeto

---
+++ src/main/java/com/example/demo/MsEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEntregaApplication.class, args);
+	}
+
+}
